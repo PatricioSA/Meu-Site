0798d97e5574c5b4094d2f7ae662a3deba124850
Patrício Samuel <patricioanjosjf@gmail.com>
2024-08-20 06:38:04 -0300

    update: responsividade mobile

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const menuButton = document.getElementById('menu-button')
 menuButton.addEventListener('click', toggleMenu)
-console.log(menuButton)
 
 function toggleMenu() {
     document.querySelector('.mobile-nav').classList.toggle('active')
